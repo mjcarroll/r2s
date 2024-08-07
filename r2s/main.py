3ba5a1b9d82d44b70e58aadc8e80fced55f9a729
Michael Carroll <michael@openrobotics.org>
2024-08-06 21:16:36 -0500

    @wip
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- r2s/main.py
+++ r2s/main.py
@@ -1,24 +1,62 @@
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.containers import Horizontal
-from textual.widgets import Static, Footer, Input, ContentSwitcher
+from textual.containers import Horizontal, Vertical
+from textual.widgets import Input, ContentSwitcher, DataTable
+from textual.widget import Widget
 
 from textual import events
 from textual import log
 
+from rich.text import Text
+
 from r2s.modals import HelpModal
+
+class NodeWidget(Widget):
+    DEFAULT_CSS = """
+        NodeWidget {
+            height: 100%;
+            width: 100%;
+        } 
+    """
+    def compose(self) -> ComposeResult:
+        table: DataTable[Text] = DataTable(
+            id = "node_table",
+            header_height = 1,
+            show_cursor = True,
+            zebra_stripes = True,
+        )
+        table.focus()
+        table.add_column('ns')
+        table.add_column('node')
+
+        table.add_row(Text('foo', justify='left'), Text('bar'))
+        table.add_row(Text('foo', justify='left'), Text('baz'))
+        table.cursor_type='row'
+        table.ordered_columns[0].content_width=50
+        yield table
 
 
 class AppHeader(Horizontal):
-    BORDER_TITLE = "r2s - a ROS 2 terminal user interface"
+    def on_mount(self):
+        self.styles.border = (None, None)
 
-class Body(Horizontal):
+
+class Body(Vertical):
     BORDER_TITLE = "Nodes (all) [0]"
+
+    def on_mount(self):
+        self.styles.border = ("solid", "white")
+
+    def compose(self) -> ComposeResult:
+        yield NodeWidget()
+
 
 class r2sApp(App):
     TITLE = "r2s"
     SUBTITLE = "TUI for ros2cli"
     CSS_PATH = "ui.tcss"
+    ENABLE_COMMAND_PALETTE = False
+
 
     BINDINGS = [
         Binding("ctrl+c,q", "quit", "Quit", show=True, key_display="Q"),
@@ -33,6 +71,7 @@ class r2sApp(App):
        self.command = self.get_widget_by_id("command")
        self.search.disabled = True
        self.command.disabled = True
+
     
     def compose(self):
         yield AppHeader(id="header")
@@ -40,7 +79,6 @@ class r2sApp(App):
             yield Input(id="search", placeholder="Search text...")
             yield Input(id="command", placeholder="Command text...")
         yield Body(id="body")
-        yield Footer()
 
     def action_help(self) -> None:
         self.push_screen(HelpModal())
