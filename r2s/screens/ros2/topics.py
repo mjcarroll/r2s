1a84fd4f61e143baf579d2caf1ec72fccbaf5682
Jenn Nguyen <jennuine@intrinsic.ai>
2024-09-21 23:44:45 +0000

    added publishers and subcribers to topics screen

--- r2s/screens/ros2/topics.py
+++ r2s/screens/ros2/topics.py
@@ -17,6 +17,7 @@ from typing import List
 # TODO update to core, rather than quick debugging
 from r2s.screens.ros2.get_node import get_node
 
+from rclpy.topic_endpoint_info import TopicEndpointInfo
 
 @dataclass(frozen=True, eq=False)
 class Topic:
@@ -24,7 +25,8 @@ class Topic:
     type: str
     numPubs: int
     numSubs: int
-    # TODO add node where the topic is coming from
+    pubNodes: List[str]  # the nodes publishing to the topic
+    subNodes: List[str]  # the nodes subscribing to the topic
     # TODO namespace
 
 class TopicsFetched(Message):
@@ -39,17 +41,21 @@ class TopicListWatcher(WatcherBase):
         while not self._exit_event.is_set():
             topics: List[Topic] = []
 
-            # TODO probably want to get node from node screen
             node = get_node()
             topic_names_and_types = node.get_topic_names_and_types()
 
             # Fill list of topics
             for t in topic_names_and_types:
                 topicName = t[0]
-                numPubs = node.count_publishers(topicName)
                 numSubs = node.count_subscribers(topicName)
+                numPubs = node.count_publishers(topicName)
+                topicPubs = node.get_publishers_info_by_topic(topicName)
+                topicSubs = node.get_subscriptions_info_by_topic(topicName)
+                pubNodes = [topicPub.node_name for topicPub in topicPubs]
+                subNodes = [topicSub.node_name for topicSub in topicSubs]
+
                 topics.append(
-                    Topic(name=topicName, type=t[1], numPubs=numPubs, numSubs=numSubs)
+                    Topic(name=topicName, type=t[1], numPubs=numPubs, numSubs=numSubs, pubNodes=pubNodes, subNodes=subNodes)
                 )
 
             log(topics)
@@ -57,19 +63,22 @@ class TopicListWatcher(WatcherBase):
 
 class TopicListGrid(DataGrid):
     def columns(self):
-        return ["Name", "Type", "# Pubs", "# Subs"]
+        return ["Name", "Type", "# Pubs", "# Subs", "Publishing Nodes", "Subscribing Nodes"]
 
     def on_topics_fetched(self, message: TopicsFetched) -> None:
         message.stop()
 
         table = self.query_one("#data_table", DataTable)
         for topic in message.topic_list:
+            # TODO currently this won't update when num of pub/subs changes
             if topic.name not in table.rows:
                 table.add_row(
                     topic.name,
                     topic.type,
                     topic.numPubs,
                     topic.numSubs,
+                    topic.pubNodes,  # TODO would be nice if list could show in multiline
+                    topic.subNodes,  # TODO ^^ same for multiline
                     )
 
 class TopicListScreen(Screen):
