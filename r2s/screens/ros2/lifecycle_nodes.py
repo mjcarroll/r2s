3e2cfc769ac9b29ced2932e483906d4da963631a
Jenn Nguyen <jennuine@intrinsic.ai>
2024-10-14 00:07:26 +0000

    fixed formating
    Signed-off-by: Jenn Nguyen <jennuine@intrinsic.ai>

--- r2s/screens/ros2/lifecycle_nodes.py
+++ r2s/screens/ros2/lifecycle_nodes.py
@@ -14,6 +14,7 @@ from ros2lifecycle import api as r2CycleApi
 from typing import List
 import time
 
+
 @dataclass(frozen=True, eq=False)
 class LifecycleNode:
     name: str
@@ -38,9 +39,13 @@ class LifecycleNodesListWatcher(WatcherBase):
             lifecycle_nodes: List[LifecycleNode] = []
 
             # TODO make include_hidden_nodes an option, for now default to true
-            node_names = r2CycleApi.get_node_names(node=self.node.node, include_hidden_nodes=True)
+            node_names = r2CycleApi.get_node_names(
+                node=self.node.node, include_hidden_nodes=True
+            )
 
-            states = r2CycleApi.call_get_states(node=self.node.node, node_names=[name.full_name for name in node_names])
+            states = r2CycleApi.call_get_states(
+                node=self.node.node, node_names=[name.full_name for name in node_names]
+            )
 
             # Fill list of lifecycle nodes
             for n in node_names:
@@ -48,10 +53,7 @@ class LifecycleNodesListWatcher(WatcherBase):
                 state = states[n.full_name]
 
                 lifecycle_nodes.append(
-                    LifecycleNode(
-                        name=n.full_name,
-                        state=state.label.upper()
-                    )
+                    LifecycleNode(name=n.full_name, state=state.label.upper())
                 )
             self.target.post_message(LifecycleNodesFetched(lifecycle_nodes))
             time.sleep(0.2)
@@ -64,10 +66,7 @@ class LifecycleNodesListGrid(DataGrid):
         super().__init__(id=self.id)
 
     def columns(self):
-        return [
-            "Name",
-            "State"
-        ]
+        return ["Name", "State"]
 
     def on_lifecycle_nodes_fetched(self, message: LifecycleNodesFetched) -> None:
         message.stop()
@@ -82,7 +81,6 @@ class LifecycleNodesListGrid(DataGrid):
                 )
             elif table.get_cell(lifecycle_node.name, "state") != lifecycle_node.state:
                 table.update_cell(lifecycle_node.name, "state", lifecycle_node.state)
-
 
 
 class LifecycleNodesListScreen(Screen):
